811f12cedb0be620a906cd2349ff49dfe242edd4
Matt Brauner <matt@braunermail.com>
2023-12-30 16:21:37 -0500

    Fixed for pi

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from api import get_all
 def main():
     while True:
         api_data = get_all()
-        print(f'{api_data.get('local_time')} | {api_data.get('local_date')}')
+        print(f'{api_data.get("local_time")} | {api_data.get("local_date")}')
         if 'weather' in api_data:
-            print(f'{api_data.get('weather').get('temp')}° | {api_data.get('weather').get('humidity')}%')
+            print(f'{api_data.get("weather").get("temp")}° | {api_data.get("weather").get("humidity")}%')
         if 'forecast' in api_data:
-            print(api_data.get('forecast'))
+            print(api_data.get("forecast"))
         if 'stocks' in api_data:
-            print(' | '.join([f'{stock['name']}: {stock['price']} {stock['points']} [{stock['percent']}]' for stock in api_data.get('stocks')]))
+            print(' | '.join([f'{stock["name"]}: {stock["price"]} {stock["points"]} [{stock["percent"]}]' for stock in api_data.get("stocks")]))
         print()
         time.sleep(1)
 
