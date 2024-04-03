8332bbf0740d51649b3514fca7d2e0e164847b5c
Matt Brauner <matt@braunermail.com>
2024-04-03 00:03:01 -0400

    tweak

--- pirgbsmartclock/clock.py
+++ pirgbsmartclock/clock.py
@@ -12,20 +12,13 @@ class Clock(GraphicsBase):
     COLOR2 = graphics.Color(*RGB['lightseagreen'])
 
     CLOCK_POS = (27, 13)
-    _prev_time = None
     _show_sec = True
 
-    DATE_POS = (3, 16)
-    _prev_date = None
-
+    MONTH_POS = (3, 16)
+    DATE_POS = (16, 16)
     DAY_POS = (3, 8)
-    _prev_day = None
-
     OUTDOOR_POS = (3, 27)
-    _prev_outdoor = None
-
     INDOOR_POS = (35, 27)
-    _prev_indoor = None
 
     def __init__(self, *args, **kwargs):
         super(Clock, self).__init__(*args, **kwargs)
@@ -41,7 +34,8 @@ class Clock(GraphicsBase):
     def run(self, show_clock):
         now = datetime.now()
         current_time = now.strftime("%l:%M") if self._show_sec else now.strftime("%l %M")
-        current_date = now.strftime('%-m/%-d')
+        current_month = now.strftime('%b')
+        current_date = now.strftime('%-d')
         current_day = now.strftime('%a')
         outdoor_temp = f"O-{round(WeatherApi.fetch().get('temp',0))}°"
         indoor_temp = f"I-{round(TemperApi.fetch().get('temp') * 1.8 + 32)}°"
@@ -52,24 +46,22 @@ class Clock(GraphicsBase):
         if show_clock:
             # draw clock
             graphics.DrawText(canvas, self.font1, self.CLOCK_POS[0], self.CLOCK_POS[1], self.COLOR1, current_time)
-            self._prev_time = current_time
             self._show_sec = not self._show_sec
+
+            # draw month
+            graphics.DrawText(canvas, self.font2, self.MONTH_POS[0], self.MONTH_POS[1], self.COLOR2, current_month)
 
             # draw date
             graphics.DrawText(canvas, self.font2, self.DATE_POS[0], self.DATE_POS[1], self.COLOR2, current_date)
-            self._prev_date = current_date
 
             # draw day
             graphics.DrawText(canvas, self.font2, self.DAY_POS[0], self.DAY_POS[1], self.COLOR2, current_day)
-            self._prev_day = current_day
 
             # draw outdoor conditions
             graphics.DrawText(canvas, self.font3, self.OUTDOOR_POS[0], self.OUTDOOR_POS[1], self.COLOR2, outdoor_temp)
-            self._prev_outdoor = outdoor_temp
 
             # draw indoor conditions
             graphics.DrawText(canvas, self.font3, self.INDOOR_POS[0], self.INDOOR_POS[1], self.COLOR2, indoor_temp)
-            self._prev_indoor = indoor_temp
 
 # Main function
 if __name__ == "__main__":
